9e13802bc890e7e240c9e7f8871d728a6928ee6e
Deep <deepagndhi151@gmail.com>
2021-08-16 16:16:16 +0530

    checking 2FA

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import {Link} from 'gatsby'
-import Footer from '../components/footer'
-import Header  from "../components/header";
+import Layout from "../components/layout";
 const Contact = () =>{
     return(
-        <div>
-            <Header/>
+        <Layout>
             <h1>Contact</h1>
             <p>HMU @ 9619137724</p>
             <p>Head over to my Github <a href="https://github.com/deepg7" target="_blank">Contact me</a></p>
-            <Footer/>
-        </div>
+        </Layout>
     )
 }
 
